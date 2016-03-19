1a5a2c4da0427f4bcf032f1925ea1b156e57e342
Kaori Ikada <kaori@origami.co>
2016-03-19 16:00:12 +0900

    wip: scan beacon

--- wear/src/main/java/at/dongri/android/ibeaconfinder/MainActivity.java
+++ wear/src/main/java/at/dongri/android/ibeaconfinder/MainActivity.java
@@ -3,17 +3,29 @@ package at.dongri.android.ibeaconfinder;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
 import android.support.wearable.view.BoxInsetLayout;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+
 import java.text.SimpleDateFormat;
+import java.util.Collection;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.Locale;
 
-public class MainActivity extends WearableActivity {
+public class MainActivity extends WearableActivity implements BeaconConsumer {
 
     private static final SimpleDateFormat AMBIENT_DATE_FORMAT =
             new SimpleDateFormat("HH:mm", Locale.US);
+
+    private BeaconManager mBeaconManager;
 
     private BoxInsetLayout mContainerView;
     private TextView mTextView;
@@ -28,6 +40,45 @@ public class MainActivity extends WearableActivity {
         mContainerView = (BoxInsetLayout) findViewById(R.id.container);
         mTextView = (TextView) findViewById(R.id.text);
         mClockView = (TextView) findViewById(R.id.clock);
+
+        mBeaconManager = BeaconManager.getInstanceForApplication(this);
+        mBeaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mBeaconManager.bind(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mBeaconManager.unbind(this);
+    }
+
+    @Override
+    public void onBeaconServiceConnect() {
+        Log.i("MainActirivty", "onBeaconServiceConnect");
+        mBeaconManager.setForegroundScanPeriod(500);
+        mBeaconManager.setRangeNotifier(new RangeNotifier() {
+            @Override
+            public void didRangeBeaconsInRegion(Collection<Beacon> collection, Region region) {
+                Log.i("MainActirivty", "collection size: " + collection.size());
+                if (collection.size() > 0) {
+                    for (Iterator iterator = collection.iterator(); iterator.hasNext();) {
+                        Beacon beacon = (Beacon) iterator.next();
+                        Log.i("MainActirivty", "【ibeacon】: uuid" + beacon.getId1().toString() + ", major:" + beacon.getId2().toString() + ", minor:" + beacon.getId3().toString());
+                    }
+                }
+            }
+        });
+        try {
+            Region region = new Region("findid", null, null, null);
+            mBeaconManager.startRangingBeaconsInRegion(region);
+        } catch (Exception e) {
+            Log.e("MainActirivty", e.getMessage());
+        }
     }
 
     @Override
@@ -61,4 +112,5 @@ public class MainActivity extends WearableActivity {
             mClockView.setVisibility(View.GONE);
         }
     }
+
 }
